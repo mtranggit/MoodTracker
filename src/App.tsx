357a8cff4404a005cf1d471cce984bb798d13bd2
Michael Trang <michael.trang@hotmail.com>
2022-04-14 22:12:52 +1000

    Create bottom tabs navigator

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { BottomTabsNavigator } from './screens/BottomTabs.navigator';
 
 export const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello, world!</Text>
-    </View>
+    <NavigationContainer>
+      <BottomTabsNavigator />
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'teal',
-  },
-});
